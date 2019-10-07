bff391aa78d1425a5e040c2eb3635373db8dea07
Irina <irina_4ever@mail.ru>
2019-10-07 09:12:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let getBrackets = bracketsConfig.map(item => item.join(''));
+
+      for (let i = 0; i < getBrackets.length; ) {
+          let item = getBrackets[i]
+          if (str.indexOf(item) !== -1) {
+            str = str.replace(item, '');
+            i = 0;
+          } else i++;
+      };
+      
+      return str === "" ? true : false;
+
+    console.log(getBrackets);
 }
